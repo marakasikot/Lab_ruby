c4ad95a24b94a9616a7608774d6f1927cdcecd77
marakasikot <123506628+marakasikot@users.noreply.github.com>
2024-11-12 04:01:25 +0200

    Update rental_item.rb
    Changed saving logic

--- RubyLab1-main/lib/rental_item.rb
+++ RubyLab1-main/lib/rental_item.rb
@@ -1,54 +1,12 @@
-require 'faker'
-require_relative './logger_manager'
-
+# lib/rental_item.rb
 class RentalItem
-  include Comparable
+  attr_accessor :vin_1_10, :country, :city, :state, :postal
 
-  attr_accessor :price, :address, :square_meters, :rooms, :image_path
-
-  def initialize(params = {})
-    @price = params[:price] || 0.0
-    @address = params[:address] || 'Адреса відсутня'
-    @square_meters = params[:square_meters] || 0
-    @rooms = params[:rooms] || 1
-    @image_path = params[:image_path] || 'default_image.jpg'
-
-    LoggerManager.log_processed_file("Ініціалізовано об'єкт RentalItem з адресою #{@address}")
-
-    yield(self) if block_given?
-  end
-
-  def <=>(other)
-    price <=> other.price
-  end
-
-  def to_s
-    "Оренда: Ціна: #{price} грн, Адреса: #{address}, Площа: #{square_meters} м², Кімнат: #{rooms}, Зображення: #{image_path}"
-  end
-
-  def to_h
-    instance_variables.each_with_object({}) do |var, hash|
-      hash[var.to_s.delete('@')] = instance_variable_get(var)
-    end
-  end
-
-  def inspect
-    "#<RentalItem: #{to_h}>"
-  end
-
-  alias_method :info, :to_s
-
-  def update
-    yield(self) if block_given?
-  end
-
-  def self.generate_fake
-    new(
-      price: Faker::Number.between(from: 5000, to: 20000),
-      address: Faker::Address.full_address,
-      square_meters: Faker::Number.between(from: 30, to: 200),
-      rooms: Faker::Number.between(from: 1, to: 5),
-      image_path: "images/#{Faker::Lorem.word}.jpg"
-    )
+  def initialize(vin_1_10:, country:, city:, state:, postal:)
+    @vin_1_10 = vin_1_10
+    @country = country
+    @city = city
+    @state = state
+    @postal = postal
   end
 end
