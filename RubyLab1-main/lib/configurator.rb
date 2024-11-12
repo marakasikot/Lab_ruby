644b2dee33cce7f1c9e27e9dbed723f58a5571e5
marakasikot <123506628+marakasikot@users.noreply.github.com>
2024-11-12 04:12:56 +0200

    Update configurator.rb
    Created saving logick, aded some debugging for testing

--- RubyLab1-main/lib/configurator.rb
+++ RubyLab1-main/lib/configurator.rb
@@ -1,58 +1,31 @@
-require_relative './database_connector'
-
 class Configurator
-    attr_reader :config
-  
-    def initialize
-      @config = {
-        run_website_parser: 0,
-        run_save_to_csv: 0,
-        run_save_to_json: 0,
-        run_save_to_yaml: 0,
-        run_save_to_sqlite: 0,
-        run_save_to_mongodb: 0
-      }
-    end
-  
-    def configure(overrides = {})
-      overrides.each do |key, value|
-        if @config.key?(key)
-          @config[key] = value
-        else
-          puts "Warning: #{key} is not a valid configuration key."
-        end
-      end
-    end
-  
-    def self.available_methods
-      @config.keys
-    end
-  
-    def run_actions(cart, webparsing_config, mongodb_config)
-      if @config[:run_website_parser] == 1
-        cart.start_parsing(webparsing_config)
-      end
-      puts "\nSaving items to files..."
-      
-      if @config[:run_save_to_csv] == 1
-        cart.save_to_csv
-      end
-      if @config[:run_save_to_json] == 1
-        cart.save_to_json
-      end
-      if @config[:run_save_to_yaml] == 1
-        cart.save_to_yml
-      end
-      if @config[:run_save_to_mongodb] == 1
-        db_connector = DatabaseConnector.new(mongodb_config)
-        db_connector.connect_to_database
+  def configure(run_website_parser:, run_save_to_csv:, run_save_to_json:, run_save_to_yaml:, run_save_to_mongodb:)
+    @actions = {
+      website_parser: run_website_parser,
+      save_to_csv: run_save_to_csv,
+      save_to_json: run_save_to_json,
+      save_to_yaml: run_save_to_yaml,
+      save_to_mongodb: run_save_to_mongodb
+    }
+    puts "Actions configured: #{@actions}"  # Debugging line to check the configuration
+  end
 
-        cart.items.each do |item|
-          db_connector.save_rental_item(item)
-        end
-
-        db_connector.close_connection
-      end
+  def run_actions(cart, webparsing_config, mongodb_config)
+    if @actions.nil?
+      raise "Actions were not properly configured"
+    end
+    
+    if @actions[:website_parser] == 1
+      cart.start_parsing(webparsing_config)
+    end
+    if @actions[:save_to_csv] == 1
+      cart.save_to_csv
+    end
+    if @actions[:save_to_json] == 1
+      cart.save_to_json  # Ensure you have this method implemented if you want to handle JSON saving
+    end
+    if @actions[:save_to_yaml] == 1
+      cart.save_to_yaml
     end
   end
-  
+end
